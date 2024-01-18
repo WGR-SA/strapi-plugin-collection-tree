68f33837a03cb0281bbc8b83beb508a15c6720b3
jeanvier <damien.grossfeld@gmail.com>
2024-01-18 17:00:38 +0100

    fix: sort locale path

--- admin/src/components/SortLocaleSelect/index.tsx
+++ admin/src/components/SortLocaleSelect/index.tsx
@@ -10,7 +10,9 @@ import { useLocation } from 'react-router-dom'
 import { SingleSelect, SingleSelectOption } from '@strapi/design-system'
 
 const SortLocaleSelect = ({ locales }: { locales: any }) => {
-  const path = useLocation().pathname
+  let path = useLocation().pathname
+  if (!path.includes('/admin')) path = '/admin' + path
+  
   const currentLocale = new URLSearchParams(useLocation().search).get('locale') ?? locales.find((locale) => locale.isDefault)?.code
 
   if (locales.length)
